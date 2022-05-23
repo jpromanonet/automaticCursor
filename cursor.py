ace8b443197d350dce958d058dd53b3a88f0dc18
jpromanonet <programming@jpromano.net>
2022-05-23 11:48:03 -0300

    Done

---
+++ cursor.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+pyautogui.click(100, 100)
+pyautogui.moveTo(100, 150)
+pyautogui.moveRel(0, 10)  # move mouse 10 pixels down
+pyautogui.dragTo(100, 150)
+pyautogui.dragRel(0, 10)  # drag mouse 10 pixels down
+
+pyautogui.click(50, 50)
+pyautogui.moveTo(50, 75)
+pyautogui.moveRel(0, 5)  # move mouse 10 pixels down
+pyautogui.dragTo(50, 75)
+pyautogui.dragRel(0, 5)  # drag mouse 10 pixels down
